77cb5b76d6b8092dd7c6a6def2f8f96d21d5b8b9
Asmafarhat01 <asmafarhatshaik786@gmail.com>
2025-11-20 14:03:15 -0600

    initial commit: Flask app with docker

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return '''
+    <h1>Automated Deployement Pipeline</h1>
+    <p>This Flask app is deployed using docker and Github Actions CI/CD!</p>
+    <p>Built By Asma Farhat Shaik</p>
+    '''
+@app.route('/health')
+def health():
+    return {'status': 'healthy'}, 200
+if __name__ == '__main__':
+    app.run(host= '0.0.0.0', port=5000, debug=True)
